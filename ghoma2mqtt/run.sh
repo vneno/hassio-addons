031b98083cb112728f23c1d537bb5a58cebb05b8
Peter Vnencak <peter@vnencak.com>
2023-01-22 22:07:06 +0100

    internal_mqtt_srv

--- ghoma2mqtt/run.sh
+++ ghoma2mqtt/run.sh
@@ -1,19 +1,40 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 set -e
 
 CONFIG_PATH=/data/options.json
 
-# parse inputs from options
-MQTTHOST=$(jq --raw-output ".MQTTHOST" $CONFIG_PATH)
-MQTTPORT=$(jq --raw-output ".MQTTPORT" $CONFIG_PATH)
-MQTTSSL=$(jq --raw-output ".MQTTSSL" $CONFIG_PATH)
-MQTTUSER=$(jq --raw-output ".MQTTUSER" $CONFIG_PATH)
-MQTTPASS=$(jq --raw-output ".MQTTPASS" $CONFIG_PATH)
-RETAIN=$(jq --raw-output ".RETAIN" $CONFIG_PATH)
-AUTODISCOVERY=$(jq --raw-output ".AUTODISCOVERY" $CONFIG_PATH)
-DISCOVERYTOPIC=$(jq --raw-output ".DISCOVERYTOPIC" $CONFIG_PATH)
-DEBUG=$(jq --raw-output ".DEBUG" $CONFIG_PATH)
 
-echo "---"
-echo "starting ghoma2mqtt"
+MQTT_HOST=$(bashio::config 'mqtt_host')
+if [[ -z "$MQTT_HOST" || $MQTT_HOST == "null" ]]; then 
+    if ! bashio::services.available "mqtt"; then
+        bashio::log.error "No internal MQTT service found. Please install the internal Mosquitto MQTT broker integration."
+    else
+        bashio::log.info "Internal MQTT service found, fetching configuration ..."
+        MQTT_HOST=$(bashio::services mqtt "host")
+        MQTT_USER=$(bashio::services mqtt "username")
+        MQTT_PASSWORD=$(bashio::services mqtt "password")
+
+        RETAIN=$(jq --raw-output ".retain" $CONFIG_PATH)
+        AUTODISCOVERY=$(jq --raw-output ".autodiscovery" $CONFIG_PATH)
+        DISCOVERYTOPIC=$(jq --raw-output ".discoverytopic" $CONFIG_PATH)
+        DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH)
+
+    fi
+else
+    # parse inputs from options
+    MQTTHOST=$(jq --raw-output ".mqtt_host" $CONFIG_PATH)
+    MQTTPORT=$(jq --raw-output ".mqtt_port" $CONFIG_PATH)
+    MQTTSSL=$(jq --raw-output ".mqtt_ssl" $CONFIG_PATH)
+    MQTTUSER=$(jq --raw-output ".mqtt_user" $CONFIG_PATH)
+    MQTTPASS=$(jq --raw-output ".mqtt_pass" $CONFIG_PATH)
+    RETAIN=$(jq --raw-output ".retain" $CONFIG_PATH)
+    AUTODISCOVERY=$(jq --raw-output ".autodiscovery" $CONFIG_PATH)
+    DISCOVERYTOPIC=$(jq --raw-output ".discoverytopic" $CONFIG_PATH)
+    DEBUG=$(jq --raw-output ".debug" $CONFIG_PATH)
+
+fi
+bashio::log.info "Done."
+
+bashio::log.info "Starting python script..."
 python3 ghoma2mqtt_p3.py $MQTTHOST $MQTTPORT $MQTTSSL $MQTTUSER $MQTTPASS $RETAIN $AUTODISCOVERY $DISCOVERYTOPIC $DEBUG
+bashio::log.info "Done."
